0b98a608b15dfba302c25566b169ec83dc113e35
Folamomo <noga.igor@gmail.com>
2018-01-09 23:24:21 +0100

    raw sounds and better colision cod

--- Flipper.py
+++ Flipper.py
@@ -14,7 +14,7 @@ import pygame.gfxdraw
 class Options(object):
     def __init__(self):
         self.g=50
-        self.speed=3
+        self.speed=0.5
         self.ball_start=(200, 400)
         self.ball_speed=(100, -100)
         self.drawpolygons=False
@@ -52,7 +52,7 @@ class Material(object):
        # self.sound=load_sound(bouncesound)
         self.bouncines=bouncines
         
-steel=Material("steelsound", 0.5)
+steel=Material("steelsound", 0.7)
 
 class Edge(pygame.sprite.Sprite):
     def __init__ (self, start, end, material=steel):
@@ -84,7 +84,7 @@ class Circle(pygame.sprite.Sprite):
         self.material=material
         self.rect=pygame.Rect(self.position.x-self.radius, self.position.y-self.radius, self.position.x+self.radius, self.position.y+self.radius)
         if options.drawshapes == True:
-            pygame.gfxdraw.circle(background, int(self.position.x), int(self.position.y), int(self.radius), (255, 255, 255))           
+            pygame.gfxdraw.circle(background, int(self.position.x), int(self.position.y), int(self.radius)+1, (255, 255, 255))           
 class Polygon (pygame.sprite.Group):
     def __init__(self, vertices, material=steel):
         self.edges=[]
@@ -92,7 +92,7 @@ class Polygon (pygame.sprite.Group):
         self.material=material
         for i in range(len(vertices)):
             self.edges.append(Edge(vertices[i-1], vertices[i], self.material))
-            self.vertices.append(Circle(vertices[i], 0.5, self.material))
+            self.vertices.append(Circle(vertices[i], 0, self.material))
         if options.drawpolygons==True:
             pygame.gfxdraw.polygon(background, vertices, (255, 255, 255))
             
@@ -107,14 +107,15 @@ class Ball(pygame.sprite.Sprite):
     def bounce (self, normal, material):
         if self.velocity.angle_to(normal)>90 or self.velocity.angle_to(normal)<-90:
             self.velocity-=normal*self.velocity.dot(normal)*(1+material.bouncines) #using analitic geometry (magic)
-            print (material.bouncines)
+     
     def colision_detection (self, coliders): 
         for colider in coliders.sprites(): 
             if type(colider) is Edge:
                 if self.radius > colider.normal.x*(self.position.x-colider.start.x)+colider.normal.y*(self.position.y-colider.start.y): #distance from enge<radius
                     if 0<colider.normal.y*(self.position.x-colider.start.x)-colider.normal.x*(self.position.y-colider.start.y):         #inside box
                         if 0<-colider.normal.y*(self.position.x-colider.end.x)+colider.normal.x*(self.position.y-colider.end.y):
-                            self.bounce(colider.normal, colider.material)
+                            if 0 < colider.normal.x*(self.position.x-colider.start.x)+colider.normal.y*(self.position.y-colider.start.y):
+                                self.bounce(colider.normal, colider.material)
                             
             elif type(colider) is Circle: 
                 if self.radius+colider.radius>(self.position-colider.position).length():
@@ -149,14 +150,17 @@ balls = pygame.sprite.Group()
 coliders=pygame.sprite.Group()
     # Initialise ball
 ball1 = Ball(options.ball_start,options.ball_speed)
-ball2 = Ball((120, 60), (0, 30))
+ball2 = Ball((120, 115), (70, 0))
     # Initialise clock
 clock = pygame.time.Clock()
     #initialise field
-edges=Polygon([(50 , 0) , (500, 50), (550, 600), (55, 400)])
+edges=Polygon([(50 , 0) , (500, 50), (550, 500), (55, 400)])
 line=Polygon([ (500, 500), (400, 500)])
-#square=Polygon([(201 , 200) , (200, 101), (101, 100), (100, 201)])
+square=Polygon([(201 , 200) , (200, 101), (101, 100), (100, 201)])
 circle1=Circle((100, 200), 55)
+circle2=Circle((300, 310), 0)
+square2=Polygon([(250, 250), (250, 300), (300, 300), (300, 250)])
+ball3=Ball((320, 230), (-68.356, 50))
     # Blit everything to the screen
 screen.blit(background, (0, 0))
 pygame.display.flip()
@@ -165,7 +169,7 @@ quit=False
 
 while quit is not True:
     
-    dt=clock.tick()/1000     #frame length in seconds
+    dt=clock.tick(60)/1000     #frame length in seconds
    # print(clock.get_fps())
     
     for event in pygame.event.get():
